ff3793ffe9dff4e6abe6a957cc4b37fab76b258e
KE LI <like19911209@hotmail.com>
2020-12-19 10:00:54 +0900

    fix: Change main page grid number

--- src/pages/MainPage/styled.js
+++ src/pages/MainPage/styled.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export const MainContent = styled.div`
   padding-top: 170px;
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   grid-gap: 10px;
 `;
