b4b00553275fce98f9f602c859bfd53837fb75a7
KE LI <like19911209@hotmail.com>
2020-12-20 21:29:54 +0900

    feat: Change loading modalcontent with gif

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { StyledH1 } from './styled';
+import { WackyTubeMan } from './styled';
 
 export default function Loading() {
   return (
-    <StyledH1>
-      Loading...
-    </StyledH1>
+    <WackyTubeMan />
   );
 }
--- src/components/Loading/styled.js
+++ src/components/Loading/styled.js
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
+import { logo } from '../../constants/image';
 
 export const StyledH1 = styled.h1`
   color: ${({theme}) => theme.palette.WHITE};
 `;
 
+export const WackyTubeMan = styled.div`
+  background-image: url(${logo.WACKY_TUBE_MAN});
+  background-size: contain;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 12%;
+  left: 22%;
+  width: 100%;
+  height: 80%;
+  z-index: 9999;
+`;
